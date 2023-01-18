d02fcbd1ecd4c551047dc37c5d2d3406f097e25c
Lucaomena <lucaomena32@gmail.com>
2023-01-18 20:21:46 -0300

    Primeiro commit

---
+++ JavaTerminal/src/AboutMe.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //Impressão de dados
+        System.out.println("ola, me chamo " + nome + " " + sobrenome + ",");
+        System.out.println("Minha idade é: " + idade);
+        System.out.println("tenho " + altura + "cm's ");
+        
+    }
+    
+}
